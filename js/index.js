c17427413665d249de9424b8dd4e542e01f18091
karapeoples <52808542+karapeoples@users.noreply.github.com>
2019-10-30 22:17:36 -0500

    Commit before Starting Task 3 Because: Code Resting Place before StandUp Code Done with Next Task Also before filling out a Report on How much I have accomplished This commit: Nav Reconfigured to be better code All of the Premade content is on the page Good Pull State in Case Task 3 cause too many errors

--- js/index.js
+++ js/index.js
@@ -46,25 +46,62 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute("src", siteContent["nav"]["img-src"]);
 
-const service = (document.getElementsByTagName("a")[0].textContent =
-	siteContent.nav["nav-item-1"]);
-const product = (document.getElementsByTagName("a")[1].textContent =
-	siteContent.nav["nav-item-2"]);
-const vision = (document.getElementsByTagName("a")[2].textContent =
-	siteContent.nav["nav-item-3"]);
-const feature = (document.getElementsByTagName("a")[3].textContent =
-	siteContent.nav["nav-item-4"]);
-const about = (document.getElementsByTagName("a")[4].textContent =
-	siteContent.nav["nav-item-5"]);
-const contact = (document.getElementsByTagName("a")[5].textContent =
-	siteContent.nav["nav-item-6"]);
+//# NavSection
 
-let ctaImg = document.getElementById("cta-img");
+const links = document.querySelectorAll("nav a");
+const linksArr = Array.from(links);
+linksArr[0].textContent = siteContent["nav"]["nav-item-1"];
+linksArr[1].textContent = siteContent["nav"]["nav-item-2"];
+linksArr[2].textContent = siteContent["nav"]["nav-item-3"];
+linksArr[3].textContent = siteContent["nav"]["nav-item-4"];
+linksArr[4].textContent = siteContent["nav"]["nav-item-5"];
+linksArr.forEach = color
+
+//$ CTA Section
+
+const ctaImg = document.getElementById("cta-img");
 ctaImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
-const ctaTitle = (document.querySelector("h1").textContent =
-	siteContent.cta["h1"]);
-const button = (document.querySelector("button").textContent =
-	siteContent.cta["button"]);
+const ctaTitle = document.querySelector(".cta-text h1");
+ctaTitle.innerHTML= siteContent["cta"]["h1"].replace(/\s/g, "<br />");
+//$figure out how to do a break with .textContent in the place of InnerHTML
 
-	
+const ctaButton = document.querySelector(".cta-text button");
+ctaButton.textContent =
+  siteContent.cta["button"];
+  
+
+  //!Main-Content Section
+
+  const midImg =document.getElementById("middle-img");
+  midImg.setAttribute("src", siteContent["main-content"]["middle-img-src"])
+
+  const sectionHead = document.querySelectorAll(".main-content h4");
+  sectionHead[0].textContent = siteContent["main-content"]["features-h4"]
+  sectionHead[1].textContent = siteContent["main-content"]["about-h4"]
+  sectionHead[2].textContent = siteContent["main-content"]["services-h4"]
+  sectionHead[3].textContent = siteContent["main-content"]["product-h4"]
+  sectionHead[4].textContent = siteContent["main-content"]["vision-h4"]
+
+  const sectionInfo = document.querySelectorAll(".main-content p");
+  sectionInfo[0].textContent =siteContent["main-content"]["features-content"]
+  sectionInfo[1].textContent =siteContent["main-content"]["about-content"]
+  sectionInfo[2].textContent =siteContent["main-content"]["services-content"]
+  sectionInfo[3].textContent =siteContent["main-content"]["product-content"]
+  sectionInfo[4].textContent =siteContent["main-content"]["vision-content"]
+
+//% Contact Section
+
+const contactTitle = document.querySelector(".contact h4");
+contactTitle.textContent = siteContent.contact["contact-h4"]
+
+const contactInfo = document.querySelectorAll(".contact p")
+const contactArr = Array.from(contactInfo)
+contactArr[0].textContent=siteContent["contact"]["address"]
+contactArr[1].textContent=siteContent["contact"]["phone"];
+contactArr[2].textContent=siteContent["contact"]["email"];
+
+//! Footer Section
+
+const footerInfo = document.querySelector("footer p");
+footerInfo.textContent=siteContent["footer"]["copyright"]
